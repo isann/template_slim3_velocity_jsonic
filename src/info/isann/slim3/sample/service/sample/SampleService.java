b3708bb0a2ce68bbdd68ffe1b1676b7194fbfde4
Zono Tom <issann0330@hotmail.com>
2011-10-16 02:34:04 +0900

    modify test.

--- src/info/isann/slim3/sample/service/sample/SampleService.java
+++ src/info/isann/slim3/sample/service/sample/SampleService.java
@@ -3,7 +3,7 @@ package info.isann.slim3.sample.service.sample;
 public class SampleService {
 
     public String getMessage(){
-        return "サンプルサービスからのメッセージです";
+        return "サンプルサービスからのメッセージです.";
     }
     
 }
